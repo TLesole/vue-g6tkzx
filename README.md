4f26f0e110a86b4a986098341f39cdf8d85b04a1
TLesole <219070016@mycput.ac.za>
2022-02-09 09:32:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-g6tkzx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-g6tkzx)
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+    <h2>Welcome to my first VUE.js project
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: serif;
+  text-align: center;
+  color: #800080;
+  margin-top: 60px;
+}
+</style>
